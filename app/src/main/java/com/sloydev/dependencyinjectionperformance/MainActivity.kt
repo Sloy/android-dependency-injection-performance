9227fe904d409cd49ef95685b93623b378a5fb80
Michael Roebuck <coachroebuck@yahoo.com>
2019-08-23 03:03:36 -0600

    I actually want to display the performance test results on the device, and have the option to repeat those tests. (#8)

--- app/src/main/java/com/sloydev/dependencyinjectionperformance/MainActivity.kt
+++ app/src/main/java/com/sloydev/dependencyinjectionperformance/MainActivity.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var job: Deferred<Unit>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,11 +19,23 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            runTests()
         }
 
-        InjectionTest().runTests()
+        runTests()
+    }
+
+    private fun runTests() {
+        runOnUiThread {
+            textView.text = getString(R.string.running_tests)
+        }
+        this.job = GlobalScope.async {
+            InjectionTest().runTests {
+                runOnUiThread {
+                    textView.text = it
+                }
+            }
+        }
     }
 
 }
