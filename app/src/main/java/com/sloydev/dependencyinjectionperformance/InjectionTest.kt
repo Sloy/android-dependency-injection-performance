556f478d6a287bf5de9f1eb0b1b082356f3ab5b2
Rafa Vázquez <rafa.vazsan@gmail.com>
2021-07-13 11:59:01 +0200

    Koin 3

--- app/src/main/java/com/sloydev/dependencyinjectionperformance/InjectionTest.kt
+++ app/src/main/java/com/sloydev/dependencyinjectionperformance/InjectionTest.kt
@@ -4,43 +4,21 @@ import android.os.Build
 import com.sloydev.dependencyinjectionperformance.custom.DIContainer
 import com.sloydev.dependencyinjectionperformance.custom.customJavaModule
 import com.sloydev.dependencyinjectionperformance.custom.customKotlinModule
-import com.sloydev.dependencyinjectionperformance.dagger2.DaggerJavaDaggerComponent
-import com.sloydev.dependencyinjectionperformance.dagger2.DaggerKotlinDaggerComponent
-import com.sloydev.dependencyinjectionperformance.dagger2.JavaDaggerComponent
-import com.sloydev.dependencyinjectionperformance.dagger2.KotlinDaggerComponent
-import com.sloydev.dependencyinjectionperformance.katana.katanaJavaModule
-import com.sloydev.dependencyinjectionperformance.katana.katanaKotlinModule
-import com.sloydev.dependencyinjectionperformance.kodein.kodeinKotlinModule
-import com.sloydev.dependencyinjectionperformance.kodein.kodeinJavaModule
 import com.sloydev.dependencyinjectionperformance.koin.koinJavaModule
 import com.sloydev.dependencyinjectionperformance.koin.koinKotlinModule
-import org.kodein.di.Kodein
-import org.kodein.di.direct
-import org.kodein.di.erased.instance
-import org.koin.core.KoinComponent
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.get
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
-import org.koin.core.get
-import org.rewedigital.katana.Component
-import org.rewedigital.katana.Katana
-import org.rewedigital.katana.android.environment.AndroidEnvironmentContext
-import org.rewedigital.katana.android.environment.AndroidEnvironmentContext.Profile.SPEED
-import javax.inject.Inject
 
 class InjectionTest : KoinComponent {
-
-    private val kotlinDaggerTest = KotlinDaggerTest()
-    private val javaDaggerTest = JavaDaggerTest()
 
     private val rounds = 100
 
     fun runTests(): List<LibraryResult> {
         val results = listOf(
             koinTest(),
-            kodeinTest(),
-            katanaTest(),
-            customTest(),
-            daggerTest()
+            customTest()
         )
         reportMarkdown(results)
         return results
@@ -54,7 +32,11 @@ class InjectionTest : KoinComponent {
         log("Library | Setup Kotlin | Setup Java | Inject Kotlin | Inject Java")
         log("--- | ---:| ---:| ---:| ---:")
         results.forEach {
-            log("**${it.injectorName}** | ${it[Variant.KOTLIN].startupTime.median().format()} | ${it[Variant.JAVA].startupTime.median().format()}  | ${it[Variant.KOTLIN].injectionTime.median().format()} | ${it[Variant.JAVA].injectionTime.median().format()}")
+            log(
+                "**${it.injectorName}** | ${it[Variant.KOTLIN].startupTime.median().format()} | ${
+                    it[Variant.JAVA].startupTime.median().format()
+                }  | ${it[Variant.KOTLIN].injectionTime.median().format()} | ${it[Variant.JAVA].injectionTime.median().format()}"
+            )
         }
     }
 
@@ -95,37 +77,6 @@ class InjectionTest : KoinComponent {
         ))
     }
 
-    private fun kodeinTest(): LibraryResult {
-        log("Running Kodein...")
-        lateinit var kodein: Kodein
-        return LibraryResult("Kodein", mapOf(
-            Variant.KOTLIN to runTest(
-                setup = { kodein = Kodein { import(kodeinKotlinModule) } },
-                test = { kodein.direct.instance<Fib8>() }
-            ),
-            Variant.JAVA to runTest(
-                setup = { kodein = Kodein { import(kodeinJavaModule) } },
-                test = { kodein.direct.instance<FibonacciJava.Fib8>() }
-            )
-        ))
-    }
-
-    private fun katanaTest(): LibraryResult {
-        log("Running Katana...")
-        Katana.environmentContext = AndroidEnvironmentContext(profile = SPEED)
-        lateinit var component: Component
-        return LibraryResult("Katana", mapOf(
-            Variant.KOTLIN to runTest(
-                setup = { component = Component(modules = listOf(katanaKotlinModule)) },
-                test = { component.injectNow<Fib8>() }
-            ),
-            Variant.JAVA to runTest(
-                setup = { component = Component(modules = listOf(katanaJavaModule)) },
-                test = { component.injectNow<FibonacciJava.Fib8>() }
-            )
-        ))
-    }
-
     private fun customTest(): LibraryResult {
         log("Running Custom...")
         return LibraryResult("Custom", mapOf(
@@ -140,31 +91,5 @@ class InjectionTest : KoinComponent {
                 teardown = { DIContainer.unloadModules() }
             )
         ))
-    }
-
-    private fun daggerTest(): LibraryResult {
-        log("Running Dagger...")
-        lateinit var kotlinComponent: KotlinDaggerComponent
-        lateinit var javaComponent: JavaDaggerComponent
-        return LibraryResult("Dagger", mapOf(
-            Variant.KOTLIN to runTest(
-                setup = { kotlinComponent = DaggerKotlinDaggerComponent.create() },
-                test = { kotlinComponent.inject(kotlinDaggerTest) }
-            ),
-            Variant.JAVA to runTest(
-                setup = { javaComponent = DaggerJavaDaggerComponent.create() },
-                test = { javaComponent.inject(javaDaggerTest) }
-            )
-        ))
-    }
-
-    class KotlinDaggerTest {
-        @Inject
-        lateinit var daggerFib8: Fib8
-    }
-
-    class JavaDaggerTest {
-        @Inject
-        lateinit var daggerFib8: FibonacciJava.Fib8
     }
 }
